b2ade2b3e77603d36ee55501e78abc0b0e33a4ad
Michel0009 <michaellutfallah007@gmail.com>
2026-06-16 03:43:51 +0300

    FIXES6

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,5 +14,6 @@ fi
 php /var/www/html/artisan migrate --force || echo "Migration warning (non-fatal)"
 php /var/www/html/artisan config:cache
 php /var/www/html/artisan route:cache
+php /var/www/html/artisan db:show --no-ansi 2>&1 || echo "DB CONNECTION FAILED"
 # Hand over execution to supervisor to manage PHP-FPM and Nginx
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
